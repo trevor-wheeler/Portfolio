07ec4cff7767ce312a326aa810ad3b4541c6d23f
Trevor Wheeler <trevoraustinwheeler@gmail.com>
2024-09-05 18:36:00 -0700

    Add animations, add summary

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,294 @@
-import Nav from './navbar.jsx'
-import Home from './pages/home.jsx'
-import Projects from './pages/projects.jsx'
-import Contact from './pages/contact.jsx'
-import Confirm from './pages/confirm.jsx'
-import { Route, Routes } from "react-router-dom"
+import pfp from "/pfp.png"
+import React, { useContext, useState, useRef, useEffect } from 'react'
+import { useLocation, Route, Routes, NavLink, useNavigate } from "react-router-dom"
+import { annotate, annotationGroup } from 'rough-notation';
+
+const pageDataContext = React.createContext();
+
+const clickSound = new Audio('/click6.mp3');
+const soundEffect = new Audio('/switch.mp3');
 
 export default function App() {
+  const page = useRef();
+  const nav = useRef();
+  const location = useLocation();
+
+  useEffect(() => {
+    animate(nav, page);
+  }, [location.pathname]);
+
+  return (
+    <pageDataContext.Provider value={{page, nav}}>
+      <Nav />
+      <main>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+    </pageDataContext.Provider>
+  );
+}
+
+// Navbar
+function Nav() {
+  const {page} = useContext(pageDataContext);
+  const {nav} = useContext(pageDataContext);
+
+  return (
+    <nav ref={nav}>
+      <ul className="nav-routes">
+        <li><NavLink to="/">About</NavLink></li>
+        <li><NavLink to="/projects">Projects</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+      </ul>
+      <button className="accent theme-btn gradient" onClick={() => updateTheme(page, nav)}><i className="bi bi-moon-stars-fill"></i></button>
+    </nav>
+  );
+}
+
+function animate(nav, page) {
+  let array = Array.from(nav.current.querySelectorAll('.accent'));
+  array = array.concat(Array.from(page.current.querySelectorAll('.accent')));
+  
+  array.forEach(element => {
+    element.classList.remove('active');
+    setTimeout(() => element.classList.add('active'), 10);
+  })
+}
+
+function updateTheme(page, nav) {
+  animate(nav, page);
+
+  soundEffect.currentTime = 0;
+  soundEffect.play();
+  let theme = localStorage.getItem('theme');
+  // If theme is dark set theme to light
+  if (theme === 'dark') {
+    document.body.classList = 'light';
+    localStorage.setItem('theme', 'light');
+  }
+  // If theme is light set theme to dark
+  else if (theme === 'light') {
+    document.body.classList = 'dark';
+    localStorage.setItem('theme', 'dark');
+  }
+}
+
+// About
+function About() {
+  const {page} = useContext(pageDataContext);
+  const paragraph = useRef();
+  useEffect(() => {
+    const text = paragraph.current.querySelectorAll('.highlight');
+    var group = [];
+
+    text.forEach((word, index) => {
+      let annotation = annotate(word, {type: 'underline', multiline: true, animationDuration: 100});
+      annotation.color = 'var(--highlight)';
+      if (index % 2 === 0) {
+        annotation.color = 'var(--highlight)';
+      }
+      else {
+        annotation.color = 'var(--highlight-accent)';
+      }
+
+      group.push(annotation);
+    });
+
+    const newgroup = annotationGroup(group);
+    setTimeout(() => newgroup.show(), 100);
+  }, []);
+
+  return (
+    <>
+      <div className="profile">
+        <img className="pfp" src={pfp} alt="Profile picture" draggable="false" />
+        <div className="links">
+          <a className="active" href="https://www.github.com/xXtrevorXx/" target="_blank"><i className="bi bi-github"></i></a>
+          <a className="active" href="https://www.linkedin.com/in/trevorwheeler/" target="_blank"><i className="bi bi-linkedin"></i></a>
+        </div>
+      </div>
+      <h1>Trevor Wheeler</h1>
+      <p className="text" ref={paragraph}>I'm a <span className="highlight">full-stack developer</span> located in the Greater <span className="highlight">Seattle</span> Area, most skilled in <span className="highlight">Python and JavaScript</span>, with a foundational <span className="highlight">background in C</span>. In addition to programming, I have <span className="highlight">experience in Linux</span> and managing web servers with <span className="highlight">Docker and AWS</span>. My goal as a developer is to build responsive, user-friendly web applications and continually expand my skill set with the <span className="highlight">newest technology</span>. Outside of programming, I have interests in music production and personal finance.</p>
+      <h1>Featured Projects</h1>
+      <div ref={page} className="projects">
+        <div className="accent project">
+          <h5 className="title">MIDI Piano</h5>
+          <p className="text">A web based piano application that can be controlled with either a midi controller or a mouse.</p>
+          <div className="tags">
+            <h6 className="title">Tags:</h6>
+            <span className="tag">HTML</span>
+            <span className="tag">CSS</span>
+            <span className="tag">Javascript</span>
+            <span className="tag">Python</span>
+            <span className="tag">Django</span>
+            <span className="tag">Tone.js</span>
+            <span className="tag">WEBMIDI.js</span>
+            <span className="tag">Postgres</span>
+          </div>
+          <div className="project-links">
+            <a href="https://github.com/xXtrevorXx/MIDI-Piano" target="_blank"><i className="bi bi-github"></i></a>
+            <a href="https://www.youtube.com/watch?v=Kc5otcJNYwY" target="_blank"><i className="bi bi-youtube"></i></a>
+            <a href="/midi" target="_blank"><i className="bi bi-link-45deg"></i></a>
+          </div>
+        </div>
+        <div className="accent project">
+          <h5 className="title">Portfolio Site</h5>
+          <p className="text">A website made to demonstrate my skills as a developer and act as a hub for my personal projects.</p>
+          <div className="tags">
+            <h6 className="title">Tags:</h6>
+            <span className="tag">HTML</span>
+            <span className="tag">CSS</span>
+            <span className="tag">Javascript</span>
+            <span className="tag">React</span>
+            <span className="tag">Docker</span>
+            <span className="tag">AWS</span>
+          </div>
+          <div className="project-links">
+            <a href="https://github.com/xXtrevorXx/Chrome-Extension" target="_blank"><i className="bi bi-github"></i></a>
+            <a href="https://www.youtube.com/watch?v=0" target="_blank"><i className="bi bi-youtube"></i></a>
+            <a href="/" target="_blank"><i className="bi bi-link-45deg"></i></a>
+          </div>
+        </div>
+        <div className="accent project">
+          <h5 className="title">Chrome Extension</h5>
+          <p className="text">A chrome extension for removing short-form content from websites like YouTube and Instagram.</p>
+          <div className="tags">
+            <h6 className="title">Tags:</h6>
+            <span className="tag">HTML</span>
+            <span className="tag">CSS</span>
+            <span className="tag">Javascript</span>
+            <span className="tag">Manifest</span>
+          </div>
+          <div className="project-links">
+            <a href="https://github.com/xXtrevorXx/Portfolio" target="_blank"><i className="bi bi-github"></i></a>
+            <a href="https://www.youtube.com/watch?v=ej5fKXtoeTA" target="_blank"><i className="bi bi-youtube"></i></a>
+            <a href="https://chromewebstore.google.com/detail/short-form-content-remova/bbobcnmcegmkheaimcepkmcmnaaomagn" target="_blank"><i className="bi bi-link-45deg"></i></a>
+          </div>
+        </div>
+      </div>
+      <NavLink to="/projects" onClick={() => window.scrollTo(0, 0)}>View All <i className="bi bi-arrow-right"></i></NavLink>
+    </>
+  );
+}
+
+// Contact
+function Contact() {
+  const [mailSent, setMailSent] = useState(false);
+
+  const navigate = useNavigate();
+
+  const sendEmail = event => {
+    clickSound.volume = 0.5;
+    clickSound.currentTime = 0;
+    clickSound.play();
+    event.preventDefault();
+    console.log('Mail sent');
+    setMailSent(true);
+    setTimeout(() => navigate('/'), 1500)
+  }
+
+  const {page} = useContext(pageDataContext);
+
+  if (mailSent) {
+    return <Confirm />
+  }
+  else {
     return (
-        <>
-            <Nav />
-            <main>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/projects" element={<Projects />} />
-                    <Route path="/contact" element={<Contact />} />
-                </Routes>
-            </main>
-        </>
+      <>
+        <h1>Contact</h1>
+        <form ref={page} className="contact-form" onSubmit={event => sendEmail(event)}>
+          <div className="name-box">
+            <div className="title-container first-name"><h6 className="title">First Name:</h6><span className="required">*</span></div>
+            <div className="title-container last-name"><h6 className="title">Last Name:</h6><span className="required">*</span></div>
+            <input className="input field first-name" type="text" />
+            <input className="input field last-name" type="text" />
+          </div>
+          <div className="title-container"><h6 className="title">Email:</h6><span className="required">*</span></div>
+          <input className="input field" type="text" />
+          <div className="title-container"><h6 className="title">Subject:</h6><span className="required">*</span></div>
+          <input className="input field" type="text" />
+          <div className="title-container"><h6 className="title">Body:</h6><span className="required">*</span></div>
+          <textarea className="input body"></textarea>
+          <input className="accent send-btn gradient" type="submit" value="Send" />
+        </form>
+      </>
     );
+  }
+}
+
+function Confirm() {
+  return (
+    <div className="confirmation">
+      <h3 className="confirmation-text">Sent <i className="bi bi-check-square-fill"></i></h3>
+    </div>
+  );
+}
+
+//Projects
+function Projects() {
+  const {page} = useContext(pageDataContext);
+  return (
+    <>
+      <h1>Projects</h1>
+      <div ref={page} className="projects">
+        <div className="accent project">
+          <h5 className="title">MIDI Piano</h5>
+          <p className="text">A web based piano application that can be controlled with either a midi controller or a mouse.</p>
+          <div className="tags">
+            <h6 className="title">Tags:</h6>
+            <span className="tag">HTML</span>
+            <span className="tag">CSS</span>
+            <span className="tag">Javascript</span>
+            <span className="tag">Python</span>
+            <span className="tag">Django</span>
+            <span className="tag">Tone.js</span>
+            <span className="tag">WEBMIDI.js</span>
+            <span className="tag">Postgres</span>
+          </div>
+          <div className="project-links">
+            <a href="https://github.com/xXtrevorXx/MIDI-Piano" target="_blank"><i className="bi bi-github"></i></a>
+            <a href="https://www.youtube.com/watch?v=Kc5otcJNYwY" target="_blank"><i className="bi bi-youtube"></i></a>
+            <a href="/midi" target="_blank"><i className="bi bi-link-45deg"></i></a>
+          </div>
+        </div>
+        <div className="accent project">
+          <h5 className="title">Portfolio Site</h5>
+          <p className="text">A website made to demonstrate my skills as a developer and act as a hub for my personal projects.</p>
+          <div className="tags">
+            <h6 className="title">Tags:</h6>
+            <span className="tag">HTML</span>
+            <span className="tag">CSS</span>
+            <span className="tag">Javascript</span>
+            <span className="tag">React</span>
+            <span className="tag">Docker</span>
+            <span className="tag">AWS</span>
+          </div>
+          <div className="project-links">
+            <a href="https://github.com/xXtrevorXx/Chrome-Extension" target="_blank"><i className="bi bi-github"></i></a>
+            <a href="https://www.youtube.com/watch?v=0" target="_blank"><i className="bi bi-youtube"></i></a>
+            <a href="/" target="_blank"><i className="bi bi-link-45deg"></i></a>
+          </div>
+        </div>
+        <div className="accent project">
+          <h5 className="title">Chrome Extension</h5>
+          <p className="text">A chrome extension for removing short-form content from websites like Youtube and Instagram.</p>
+          <div className="tags">
+            <h6 className="title">Tags:</h6>
+            <span className="tag">HTML</span>
+            <span className="tag">CSS</span>
+            <span className="tag">Javascript</span>
+            <span className="tag">Manifest</span>
+          </div>
+          <div className="project-links">
+            <a href="https://github.com/xXtrevorXx/Portfolio" target="_blank"><i className="bi bi-github"></i></a>
+            <a href="https://www.youtube.com/watch?v=ej5fKXtoeTA" target="_blank"><i className="bi bi-youtube"></i></a>
+            <a href="https://chromewebstore.google.com/detail/short-form-content-remova/bbobcnmcegmkheaimcepkmcmnaaomagn" target="_blank"><i className="bi bi-link-45deg"></i></a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,28 +7,26 @@ import { BrowserRouter } from 'react-router-dom'
 let theme = localStorage.getItem('theme');
 
 const getThemePreference = () => {
-    // If default theme is dark set local storage to dark
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        localStorage.setItem('theme', 'dark');
-        theme = 'dark';
-    }
-    // If default theme is light set local storage to light
-    else {
-        localStorage.setItem('theme', 'light');
-        theme = 'light';
-    }
+  // If default theme is dark set local storage to dark
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    localStorage.setItem('theme', 'dark');
+    theme = 'dark';
+  }
+  // If default theme is light set local storage to light
+  else {
+    localStorage.setItem('theme', 'light');
+    theme = 'light';
+  }
 }
 // Check for theme in local storage
 if (!theme || theme && theme === 'auto') {
-    getThemePreference();
+  getThemePreference();
 }
 // Update body to reflect theme
 document.body.classList.add(theme);
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
 )
